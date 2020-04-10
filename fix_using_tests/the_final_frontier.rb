017ea54aa098cca8f47805a3907d31120ee0bd07
James Jiang <monochrome473@gmail.com>
2020-04-10 19:32:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,11 +1,11 @@
 # don't forget to add: require 'pry'
-
 def generate_star_date
-  (rand(100000) + 400000) / 10.0
+  star_date = (rand(100000) + 400000) / 10.0
 end
 
 
-def state_log(star_date)
+def state_log
+  generate_star_date
   "Captain's Log, star date #{star_date}."
 end
 
