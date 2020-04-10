25e0a4d7d16381280c1db9351e119cf1d295a550
James Jiang <monochrome473@gmail.com>
2020-04-10 19:40:48 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,8 +1,8 @@
 # don't forget to add: require 'pry'
 require pry
-def generate_star_date
-  star_date = (rand(100000) + 400000) / 10.0
-end
+
+star_date = (rand(100000) + 400000) / 10.0
+
 
 
 def state_log(date)
